3c7e8298bb3153fd382951e1cf847b07ceca7544
gazisaiful07 <imsaiful2047@gmail.com>
2026-05-15 23:24:04 +0600

    deployment fixed

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,3 +1,7 @@
+import { betterAuth } from "better-auth";
+import Database from "better-sqlite3";
+
+
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
